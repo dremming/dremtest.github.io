689a092641706645b72f53f10c15daa5d7da1df0
dremming <dremming@mail.ru>
2018-03-26 20:17:06 +0300

    Delete palidrome.java

--- CTP/Лабораторная работа 0/palidrome.java
+++
@@ -1,1 +0,0 @@
-public class Primes {public    static void main(String[] args)    {        for (int i = 1; i <= 100; i++) {            int b = 1;            for (int j = 2; j < i; j++) {                if (i % j == 0) {                    b = 0;                    break;                }            }            if (b == 1)                System.out.print(" " + i);        }    }}
